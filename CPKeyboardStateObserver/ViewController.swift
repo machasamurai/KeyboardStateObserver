43d831b99a7b195c3cb715d6f1c64d3c3e994ac1
ramon <ramon@ramons-MacBook-Pro.local>
2016-02-05 07:38:40 +0900

    Start to implement the demo ViewController.

--- CPKeyboardStateObserver/ViewController.swift
+++ CPKeyboardStateObserver/ViewController.swift
@@ -9,17 +9,97 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var stateLabel: UILabel!
+    @IBOutlet weak var pauseButton: UIButton!
+    @IBOutlet weak var functionButton: UIButton!
+    
+    var blockForStateHide: BlockForState!
+    var blockForStateShow: BlockForState!
+    var blockForStateUndockEvent: BlockForState!
+    var blockForStateDockEvent: BlockForState!
+    var blockForStateWillMove: BlockForState!
+    var blockForStateDidMove: BlockForState!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // ブロックを用意する
+        self.initData()
+        
+        // ビューを用意する
+        self.initViews()
     }
-
+    
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        CPKeyboardStateObserver.sharedObserver.startObserving(self.view, blockForStateHide: self.blockForStateHide, blockForStateShow: self.blockForStateShow, blockForStateUndock: self.blockForStateUndockEvent, blockForStateDock: self.blockForStateDockEvent, blockForStateWillMove: self.blockForStateWillMove, blockForStateDidMove: self.blockForStateDidMove)
+    }
+    
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
+    
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        CPKeyboardStateObserver.sharedObserver.stopObserving()
+        NSNotificationCenter.defaultCenter().removeObserver(self)
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+    }
+    
+    
+    private func initData() {
+        
+        self.blockForStateHide = { (keyboardDictionary: [NSObject : AnyObject]) -> Void in
+            self.stateLabel.text = "blockDidHide"
+            self.moveLabel(keyboardDictionary, shouldFollowKeyboard: true)
+        }
+        
+        self.blockForStateShow = { (keyboardDictionary: [NSObject : AnyObject]) -> Void in
+            self.stateLabel.text = "blockDidShow"
+            self.moveLabel(keyboardDictionary, shouldFollowKeyboard: true)
+        }
+        
+        self.blockForStateUndockEvent = { (keyboardDictionary: [NSObject : AnyObject]) -> Void in
+            self.stateLabel.text = "blockDiUndock"
+            self.moveLabel(keyboardDictionary, shouldFollowKeyboard: true)
+        }
+        
+        self.blockForStateDockEvent = { (keyboardDictionary: [NSObject : AnyObject]) -> Void in
+            self.stateLabel.text = "blockDidDock"
+            self.moveLabel(keyboardDictionary, shouldFollowKeyboard: true)
+        }
+        
+        self.blockForStateWillMove = { (keyboardDictionary: [NSObject : AnyObject]) -> Void in
+            self.stateLabel.text = "blockWillMove"
+            self.moveLabel(keyboardDictionary, shouldFollowKeyboard: true)
+        }
+        
+        self.blockForStateDidMove = { (keyboardDictionary: [NSObject : AnyObject]) -> Void in
+//            self.stateLabel.text = "blockDidMove"
+            self.moveLabel(keyboardDictionary, shouldFollowKeyboard: true)
+        }
+    }
+    
+    
+    private func initViews() {
+        
     }
 
-
+    
+    private func moveLabel(userInfo: [NSObject : AnyObject], shouldFollowKeyboard: Bool) {
+        
+    }
 }
-
